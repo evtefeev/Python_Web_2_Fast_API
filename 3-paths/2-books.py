7acaabb0791959221cbb0d58490b9afabe711e32
evtefeev <nikita.evtefeev@gmail.com>
2026-06-18 17:03:33 +0300

    Refactor book model and enhance book creation endpoint

--- 3-paths/2-books.py
+++ 3-paths/2-books.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import Field
 
 
 app = FastAPI()
 
-
+next_id = 4
 books = [
     {"id": 1, "title": "1984", "author": "George Orwell"},
     {"id": 2, "title": "Brave New World", "author": "Aldous Huxley"},
     {"id": 3, "title": "To Kill a Mockingbird", "author": "Harper Lee"}
 ]
+
+class Book:
+    id: None | int = Field(None)
+    title: str = Field(..., description="Book title", min_length=3)
+    author: str = Field(..., description="Book author", min_length=3)
 
 
 @app.get("/books/")
@@ -21,4 +27,11 @@ def get_book_by_id(book_id: int):
     book = next((book for book in books if book["id"] == book_id), None)
     if book:
         return {"book": book}
-    raise HTTPException(status_code=404, detail="Книгу з таким ID не знайдено.")
+    raise HTTPException(status_code=404, detail="Книгу з таким ID не знайдено.")@app.post("/books/")
+
+async def create_book(book: Book):
+    global next_id
+    book.id = next_id
+    next_id += 1
+    books.append(book)
+    return {"message": "Нова книга була додана!", "book": book}
